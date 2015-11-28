daad8a2b1848c1c84034152386d820596cff89cf
powolnymarcel <powolnymarcel@gmail.com>
2015-11-28 23:56:54 +0100

    premier commit

--- public/app/controlleurs/principalCtrl.js
+++ public/app/controlleurs/principalCtrl.js
@@ -1,7 +1,7 @@
 angular.module('principalCtrl',[])
 
 //Le ctrl permet de manipuler les données et envoyer les données à la vue pour le rendu
-.controller('PrincipalController',function($rootScope,$location,Authentification){
+.controller('PrincipalController',function ($rootScope, $location, Authentification) {
 	var leCtrlFrontPrincipal = this;
 
 	//avoir les infos au niveau front si l'user est connecté
@@ -18,6 +18,37 @@ angular.module('principalCtrl',[])
 			});
 	});
 
-	leCtrlFrontPrincipal.
+	//a chaque click sur le bouton submit du form "faireLeLogin()"
+	leCtrlFrontPrincipal.faireLeLogin=function(){
+		leCtrlFrontPrincipal.effectuer = true;
+		leCtrlFrontPrincipal.error = '';
+
+		//Voir le factory "authentificationFactory" et la methode connexion
+		Authentification.connexion(leCtrlFrontPrincipal.donneesLogin.pseudo,leCtrlFrontPrincipal.donneesLogin.password)
+			.success(function(data){
+				leCtrlFrontPrincipal.effectuer=false;
+				Authentification.recupereUtilisateur()
+					.then(function(data){
+						leCtrlFrontPrincipal.utilisateur=data.data
+					});
+				//Si le login s'est bien passé on redirgie vers la homepage
+				if(data.success){
+					$location.path('/');
+				}else{
+					leCtrlFrontPrincipal.error= data.message
+				}
+			})
+	};
+
+	leCtrlFrontPrincipal.seDeconnecter=function(){
+		//On se deconnecte , sur le serveur on activera "assignerToken" qui activera authentificationTokenFactory.assignerToken
+		//Il y aura une condition si token ou si pas token
+		Authentification.deconnexion();
+		//On redirige
+		$location.path('/deconnexion');
+	}
+
+	//Pas de return de l'objet ici car on appelle le ctrl dans la vue,
+	//contrairement au factory qui son injectée dans les ctrl
 
 });
